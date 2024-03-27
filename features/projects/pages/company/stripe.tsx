8ec1c8000d530ea5b0a2a68b6f581f0bd1a0eaa9
AppleSeed <everlastingfriendship194@gmail.com>
2024-03-27 12:29:57 +0900

    fix:stripe

--- features/projects/pages/company/stripe.tsx
+++ features/projects/pages/company/stripe.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe('pk_test_51OV8DpHeC7VfJv8UJXLcBhECs81qBUSwD7ZJQmNuFtbien8WQCuZ2SCzkOYu2siAwkH1x4GqvCPUJqOVXQULsoz200ctmm80cL');
 
 const CheckoutPage = () => {
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         const handleClick = async () => {
+            setIsLoading(true);
             const stripe = await stripePromise;
             const { error } = await stripe.redirectToCheckout({
                 lineItems: [{ price: 'price_1Ox1PQHeC7VfJv8UA2wSiBeX', quantity: 1 }],
@@ -22,11 +24,17 @@ const CheckoutPage = () => {
     }, []);
 
     return (
-        <div>
+        <div className='pb-[5px]'>
             <div>
                 お支払いページに移行しますか？
             </div>
-            <button id="checkoutButton" className='m-[20px] px-[20px] py-[10px] rounded-[5px] text-[white] bg-[#3F8DEB] hover:bg-[#2e6fbe] hover:shadow-lg duration-500 '>確認</button>
+            <button id="checkoutButton" className='flex items-center m-auto my-[20px] px-[20px] py-[10px] rounded-[5px] text-[white] bg-[#3F8DEB] hover:bg-[#2e6fbe] hover:shadow-lg duration-500 '>
+                <img
+                    src="/img/refresh.svg"
+                    alt="rotate"
+                    className={isLoading ? "mr-[5px] rotate" : "mr-[5px] hidden"}
+                />
+                確認</button>
         </div>
     );
 };
