1cb2baeb23030aee1b284bbf9e53ed8d0f74a9ed
AppleSeed <everlastingfriendship194@gmail.com>
2024-04-20 12:41:59 +0900

    fix:database password

--- app/api/util/db.js
+++ app/api/util/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql";
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "kAZ5dB4*/x2.|mC2-4Rp",
   database: "project",
 });
 
