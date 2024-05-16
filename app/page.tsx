7799123d0f7a10c9039bff476ebdf4e6f81ebf51
AppleSeed <everlastingfriendship194@gmail.com>
2024-05-16 09:05:50 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Header from "@/components/organisms/Header";
 import Footer from "@/components/organisms/footer";
 import LoginPage from "@/features/projects/pages/LoginPage";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
