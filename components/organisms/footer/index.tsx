e3cb05199bcb82bc9c8ca8c03e37298abcaae54c
AppleSeed <everlastingfriendship194@gmail.com>
2024-05-17 13:31:51 +0900

    fix:footer

--- components/organisms/footer/index.tsx
+++ components/organisms/footer/index.tsx
@@ -1,5 +1,13 @@
-export default function Footer(){
-    return(<div className="bg-[#9E9E9E] py-[15px] text-center text-white ">
-        2023copyright
+
+// Copyright &copy; 
+// <script type="text/javascript"> 
+// myDate = new Date();myYear = myDate.getFullYear();document.write(myYear); 
+// </script>.インフルエンサーめぐり. All Rights reserved.
+
+
+export default function Footer() {
+    const year = new Date().getFullYear();
+    return (<div className="bg-[#9E9E9E] py-[15px] text-center text-white ">
+        Copyright © {year}.インフルエンサーめぐり. All Rights reserved.
     </div>)
 }
