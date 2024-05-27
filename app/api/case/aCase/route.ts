96ae0d8b1dd1f16036f3227141177ae364f68982
AppleSeed <everlastingfriendship194@gmail.com>
2024-05-27 09:40:35 +0900

    fix

--- app/api/case/aCase/route.ts
+++ app/api/case/aCase/route.ts
@@ -87,7 +87,11 @@ export async function PUT(request: NextRequest) {
       SELECT COUNT(*) AS cnt FROM apply WHERE caseId = ${id} and status = '承認'
       `;
       const count = await executeQuery(approvedInfluencerCtnQuery);
+      console.log(count[0].cnt);
+
       if (count[0].cnt === 0) {
+        console.log("here");
+
         const caseUpdateQuery = `UPDATE cases SET collectionStatus = '完了'
         WHERE id = ${id}`;
         await executeQuery(caseUpdateQuery);
