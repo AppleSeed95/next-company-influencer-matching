8554d3a8fcac59f739a2c49cb9a7da739d635a43
AppleSeed <everlastingfriendship194@gmail.com>
2024-05-28 09:31:48 +0900

    fix

--- app/api/case/aCase/route.ts
+++ app/api/case/aCase/route.ts
@@ -100,7 +100,7 @@ export async function PUT(request: NextRequest) {
         }
       );
       await executeQuery(queryWhenQuit);
-      if (count1 || (count1[0] === 0 && (!count || count[0].cnt === 0))) {
+      if ((count1 || count1[0] === 0) && (!count || count[0].cnt === 0)) {
         const caseUpdateQuery = `UPDATE cases SET collectionStatus = '完了'
         WHERE id = ${id}`;
         const result = await executeQuery(caseUpdateQuery);
