73707be57cf482d4ff2ea5d24bfbfe980f3646f5
AppleSeed <everlastingfriendship194@gmail.com>
2024-06-26 17:14:41 +0900

    fix

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -120,29 +120,29 @@ export async function POST(request: NextRequest) {
           const messageDeleteQuery = `DELETE a FROM message a 
             LEFT JOIN chatroom c ON a.roomId = c.id
             LEFT JOIN company com ON c.companyId = com.id
-            WHERE com.id = '${companyId}'`;
+            WHERE com.id = ${companyId}`;
           await executeQuery(messageDeleteQuery);
           const chatroomDeleteQuery = `DELETE c FROM chatroom c
             LEFT JOIN company com ON c.companyId = com.id
-            WHERE com.id = '${companyId}'`;
+            WHERE com.id = ${companyId}`;
           await executeQuery(chatroomDeleteQuery);
           const applyDeleteQuery = `DELETE a FROM apply a
             LEFT JOIN cases c ON a.caseId = c.id
             LEFT JOIN company com ON c.companyId = com.id
-            WHERE com.id = '${companyId}'`;
+            WHERE com.id = ${companyId}`;
           await executeQuery(applyDeleteQuery);
           // const caseDeleteQuery = `DELETE c FROM cases c
           //   LEFT JOIN company com ON c.companyId = com.id
-          //   WHERE com.id = '${companyId}'
+          //   WHERE com.id = ${companyId}
           // `;
           // await executeQuery(caseDeleteQuery);
           // const chatRoomDeleteQuery = `DELETE c FROM chatroom c
           //   LEFT JOIN company com ON c.companyId = com.id
-          //   WHERE com.id = '${companyId}'
+          //   WHERE com.id = ${companyId}
           // `;
           // await executeQuery(chatRoomDeleteQuery);
           // const companyDeleteQuery = `DELETE FROM company
-          //   WHERE company.id = '${companyId}'`;
+          //   WHERE company.id = ${companyId}`;
           // await executeQuery(companyDeleteQuery);
           // const userDeleteQuery = `DELETE FROM users
           //   WHERE id = '${user.id}'`;
